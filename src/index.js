7b27dd4a873a3f83e28c3f0b41dbf63350b99d82
sensui <sensuijunior@hotmail.com>
2020-06-28 22:42:14 -0300

    project finished

--- src/index.js
+++ src/index.js
@@ -10,16 +10,17 @@ function Square(props) {
 		>
 			{props.value}
 		</button>
-	)
+	);
 }
 
 class Board extends React.Component {
 
 	renderSquare(i) {
-		return (<Square
-			value={this.props.squares[i]}
-			onClick={() => this.props.onClick(i)}
-		/>
+		return (
+			<Square
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
+			/>
 		);
 	}
 
@@ -53,14 +54,15 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null)
 			}],
+			stepNumber: 0,
 			xIsNext: true,
 		}
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
-		const squares = this.state.squares.slice();
+		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
 			return
 		}
@@ -70,21 +72,29 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares,
 			}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0
 		});
 	}
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 
 		const moves = history.map((step, move) => {
 			const desc = move ?
-				`Go to move # ${move}` :
+				`Go to move #${move}` :
 				`Go to game start`;
 			return (
-				<li>
+				<li key={move}>
 					<button onClick={() => this.jumpTo(move)}>{desc}</button>
 				</li>
 			);
@@ -101,7 +111,7 @@ class Game extends React.Component {
 			<div className="game">
 				<div className="game-board">
 					<Board
-						squares={current.props}
+						squares={current.squares}
 						onClick={(i) => this.handleClick(i)}
 					/>
 				</div>
